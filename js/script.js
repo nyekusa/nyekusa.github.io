4d3ae1a8e193a14407c08ba668540dd6cc19add3
NYEKUSA <nyekusa01@gmail.com>
2026-06-10 16:56:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log("Club website loaded!");
+function showYear(year){
+
+    let groups =
+    document.querySelectorAll('.member-group');
+
+    groups.forEach(group=>{
+        group.style.display = 'none';
+    });
+
+    document.getElementById(year)
+    .style.display = 'grid';
+}
