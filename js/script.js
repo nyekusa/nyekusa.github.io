29d48295ec1b56cc6e4f556337b323cfab4f70ee
NYEKUSA <nyekusa01@gmail.com>
2026-06-11 15:52:23 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,31 @@ function showYear(year){
     document.getElementById(year)
     .style.display = 'grid';
 }
+const hero = document.querySelector(".hero");
+
+const images = [
+
+    "images/banner/banner1.jpg",
+    "images/banner/banner2.jpg",
+    "images/banner/banner3.jpg",
+    "images/banner/banner4.jpg"
+
+];
+
+let current = 0;
+
+function changeBanner(){
+
+    hero.style.backgroundImage =
+    `url('${images[current]}')`;
+
+    current++;
+
+    if(current >= images.length){
+        current = 0;
+    }
+}
+
+changeBanner();
+
+setInterval(changeBanner,5000);
